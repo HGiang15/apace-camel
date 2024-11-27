2b0436ee14e585ddaf5374b6cdaad380dbb491b5
dunghq23 <dungha.4work@gmail.com>
2024-11-27 08:26:13 +0700

    Done Home, config route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Home from './container/Home/Home'; 
-
+import { Routes, Route } from "react-router-dom";
+import Products from "./container/Products/Products";
+import Orders from "./container/Orders/Orders";
+import OrderDetail from "./container/OrderDetail/OrderDetail";
 
 function App() {
   return (
-    <div className="App">
-      <Home /> 
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/superset" element={<OrderDetail />} />
+    </Routes>
   );
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css'; 
 
-const Button = ({ text }) => {
+const Button = ({ text, onClick }) => {
   return (
-    <button className="btn">
+    <button className="btn" onClick={onClick}>
       <p>{text}</p>
     </button>
   );
