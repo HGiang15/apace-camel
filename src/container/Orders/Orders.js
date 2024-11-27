40cf42bf08fc125d18cb012c0aa4b11b67789998
HGiang15 <gianghoang150503@gmail.com>
2024-11-27 08:39:39 +0700

    Created-Orders

--- src/container/Orders/Orders.js
+++ src/container/Orders/Orders.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Orders.css';
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+  const orders = [
+    { id: 1, date: '26/11/2024', total: '200.000vnd' },
+    { id: 2, date: '26/11/2024', total: '200.000vnd' },
+    { id: 3, date: '26/11/2024', total: '200.000vnd' },
+    { id: 4, date: '26/11/2024', total: '200.000vnd' },
+    { id: 5, date: '26/11/2024', total: '200.000vnd' },
+    { id: 6, date: '26/11/2024', total: '200.000vnd' },
+    { id: 7, date: '26/11/2024', total: '200.000vnd' },
+    { id: 8, date: '26/11/2024', total: '200.000vnd' },
+  ];
 
-export default Orders
+  return (
+    <div className="orders-container">
+      <div className="orders-grid">
+        {orders.map((order) => (
+          <div className="order-card" key={order.id}>
+            <h3>Đơn hàng #{order.id}</h3>
+            <p>Ngày Order: {order.date}</p>
+            <p>Thành tiền: {order.total}</p>
+            <button className="order-delete">Xóa</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
